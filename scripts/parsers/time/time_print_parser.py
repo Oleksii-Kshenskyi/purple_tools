f277ffcfca53e7f209509e6d666735fd7e850f51
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2019-11-17 22:21:33 +0200

    [0.1.6] Central Purple script: - Restructured Time Print parser into file/function structure instead of class/method, as argparse does not allow for this kind of modular structure. - Functionality still BROKEN, need to understand the way subparsers are used in argparse and use them in the code properly.

--- scripts/parsers/time/time_print_parser.py
+++ scripts/parsers/time/time_print_parser.py
@@ -6,26 +6,26 @@ from scripts.utils.time.constants import TIME_PRINT_ARGUMENTS_TIME_HELP
 from scripts.utils.time.constants import UNIFORM_TIME_STRING_DEFAULT
 from scripts.time.calculation.timecalc import construct_uniform_time_string
 
-class TimePrintParser:
 
-  def __init__(self):
-    self._parser = BasicThrowingParser()
-    self._parser.description = TIME_PRINT_COMMAND_DESCRIPTION
+def create_parser():
+  parser = BasicThrowingParser()
+  parser.description = TIME_PRINT_COMMAND_DESCRIPTION
 
-    self._parser.add_argument("time", nargs="+", help=TIME_PRINT_ARGUMENTS_TIME_HELP)
+  parser.add_argument("time", nargs="+", help=TIME_PRINT_ARGUMENTS_TIME_HELP)
 
-  def __forward_number_from_argparse(self, number):
-    try:
-      return float(number)
-    except ValueError:
-      return number
+  return parser
 
+def __forward_number_from_argparse(number):
+  try:
+    return float(number)
+  except ValueError:
+    return number
 
-  def execute_command(self, arguments):
-    parse_result = self._parser.parse_args(arguments)
-    result = []
-    for item in parse_result.time:
-      item_ready = self.__forward_number_from_argparse(item)
-      result += [construct_uniform_time_string(item_ready)]
+def execute_command(parser, arguments):
+  parse_result = parser.parse_args(arguments)
+  result = []
+  for item in parse_result.time:
+    item_ready = __forward_number_from_argparse(item)
+    result += [construct_uniform_time_string(item_ready)]
 
-    return '\n'.join(result)
+  return '\n'.join(result)
--- test/unit/parsers/time/test_time_print_parser.py
+++ test/unit/parsers/time/test_time_print_parser.py
@@ -1,13 +1,12 @@
 import unittest
 
-from scripts.parsers.time.time_print_parser import TimePrintParser
+import scripts.parsers.time.time_print_parser as time_print
 from scripts.utils.time.constants import UNIFORM_TIME_STRING_DEFAULT
 
 class TestTimePrintParser(unittest.TestCase):
   
   def setUp(self):
-    self.wrapper = TimePrintParser()
-    self.parser = self.wrapper._parser
+    self.parser = time_print.create_parser()
 
   def test_parser_processes_arguments(self):
     parsing_result = self.parser.parse_args(["one"])
@@ -22,16 +21,16 @@ class TestTimePrintParser(unittest.TestCase):
       self.parser.parse_args([])
 
   def test_wrapper_executes_command(self):
-    result = self.wrapper.execute_command(["3:44:12"])
+    result = time_print.execute_command(self.parser, ["3:44:12"])
     self.assertEqual("[8.968 U @ 03:44:12]", result)
 
-    result = self.wrapper.execute_command(["00:25:00", "00:50:00", "108:64:1550"])
+    result = time_print.execute_command(self.parser, ["00:25:00", "00:50:00", "108:64:1550"])
     self.assertEqual("[1 U @ 00:25:00]\n[2 U @ 00:50:00]\n[262.793 U @ 109:29:50]", result)
 
-    result = self.wrapper.execute_command(["00:30:00", "3", "255", "148.716", "04:15:00"])
+    result = time_print.execute_command(self.parser, ["00:30:00", "3", "255", "148.716", "04:15:00"])
     self.assertEqual("[1.2 U @ 00:30:00]\n[3 U @ 01:15:00]\n[255 U @ 106:15:00]\n[148.716 U @ 61:57:54]\n[10.2 U @ 04:15:00]", result)
 
-    result = self.wrapper.execute_command(["kek", "2", "1:00:0"])
+    result = time_print.execute_command(self.parser, ["kek", "2", "1:00:0"])
     self.assertEqual(f'{ UNIFORM_TIME_STRING_DEFAULT }\n[2 U @ 00:50:00]\n[2.4 U @ 01:00:00]', result)
 
   def tearDown(self):
