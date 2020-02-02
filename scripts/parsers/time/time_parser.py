977dd277f1ade50c6bbed81365664aa50a148222
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-02-03 00:45:50 +0200

    [0.3.5] Time parser as an endpoint:
    - Switched off time_print_parser, print is a choice in the time parser itself now;
    - TODO: unit tests do not work;
    - TODO: help messages have to be properly updated;
    - TODO: time parser has to analyze the command argument, it does not yet.

--- scripts/parsers/time/time_parser.py
+++ scripts/parsers/time/time_parser.py
@@ -6,6 +6,7 @@ from scripts.utils.time.constants import TIME_COMMAND_DESCRIPTION
 from scripts.utils.time.constants import TIME_PRINT_COMMAND_DESCRIPTION
 from scripts.utils.time.constants import TIME_PRINT_ARGUMENTS_TIME_HELP
 from scripts.utils.constants import PARSER_IDENTIFIER_NAME
+from scripts.calculate.pttime import construct_uniform_time_string
 
 def name(get_short_name = False):
   return "t" if get_short_name else "time"
@@ -23,9 +24,26 @@ def create_subparser(subparsers):
 def _perform_creation(parser):
   parser.description = TIME_COMMAND_DESCRIPTION
 
-  subparsers = parser.add_subparsers(dest = PARSER_IDENTIFIER_NAME)
+  parser.add_argument("command", choices=["print", "p"])
 
-  time_print.create_subparser(subparsers)
+  parser.add_argument("time", nargs="+", help=TIME_PRINT_ARGUMENTS_TIME_HELP)
 
   return parser
 
+def execute_command(parse_result):
+  return _execute_print_subcommand(parse_result)
+
+def _execute_print_subcommand(parse_result):
+  result = []
+  for item in parse_result.time:
+    item_ready = __forward_number_from_argparse(item)
+    result += [construct_uniform_time_string(item_ready)]
+
+  return '\n'.join(result)
+
+
+def __forward_number_from_argparse(number):
+  try:
+    return float(number)
+  except ValueError:
+    return number
