4aec64152b30ab20b3cc47f39dbc908b26872b2b
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-02-04 01:49:01 +0200

    [0.3.8] New exploratory test:
    - New exploratory test on polymorphism added;
    - Made sure I understand how polymorphism works in Python.

---
+++ test/exploratory/oop/test_polymorphism.py
@@ -0,0 +1,32 @@
+import unittest
+
+class ParentClass:
+  def __init__(self):
+    self.param = "parent"
+
+  def overloaded_method(self):
+    return "CALL: parent"
+
+class ChildClass(ParentClass):
+  def __init__(self):
+    super(ChildClass, self).__init__()
+    self.parents_backed_up_param = self.param
+    self.param = "child"
+
+  def overloaded_method(self):
+    return "CALL: child"
+
+class TestPolymorphism(unittest.TestCase):
+  def setUp(self):
+    self.the_parent = ParentClass()
+    self.the_child = ChildClass()
+
+  def test_polymorphism_overloads_variables_and_methods(self):
+    self.assertEqual("parent", self.the_parent.param)
+    self.assertEqual("CALL: parent", self.the_parent.overloaded_method())
+    self.assertEqual("parent", self.the_child.parents_backed_up_param)
+    self.assertEqual("child", self.the_child.param)
+    self.assertEqual("CALL: child", self.the_child.overloaded_method())
+
+  def tearDown(self):
+    pass
