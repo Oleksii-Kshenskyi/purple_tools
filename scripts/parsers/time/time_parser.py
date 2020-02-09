ab07b8c7c162402f613717105310c00b28bf796a
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-02-10 00:42:04 +0200

    [0.5.0] Labeled uniform time strings implemented:
    - Labeled time strings have been implemented and wired up to the arparse interface;
    - Help for the optional -l/--label argument added;

--- scripts/parsers/time/time_parser.py
+++ scripts/parsers/time/time_parser.py
@@ -4,6 +4,8 @@ from scripts.parsers.base.abstract_parser import AbstractParser
 from scripts.utils.time.constants import TIME_COMMAND_DESCRIPTION
 from scripts.utils.time.constants import TIME_ARGUMENTS_TIME_HELP
 from scripts.utils.time.constants import TIME_ARGUMENTS_COMMAND_HELP
+from scripts.utils.time.constants import DEFAULT_LABEL
+from scripts.utils.time.constants import TIME_ARGUMENTS_LABEL_HELP
 from scripts.utils.constants import PARSER_IDENTIFIER_NAME
 from scripts.calculate.pttime import construct_uniform_time_string
 
@@ -14,6 +16,8 @@ class TimeParser(AbstractParser):
     self.short_name = "t"
     self.subcommand_name_print = "print"
     self.subcommand_short_name_print = "p"
+    self.optional_name_label = "--label"
+    self.optional_short_name_label = "-l"
 
   def _perform_creation(self):
     self.parser.description = TIME_COMMAND_DESCRIPTION
@@ -23,6 +27,7 @@ class TimeParser(AbstractParser):
                                   help=TIME_ARGUMENTS_COMMAND_HELP)
 
     self.parser.add_argument("time", nargs="+", help=TIME_ARGUMENTS_TIME_HELP)
+    self.parser.add_argument(self.optional_short_name_label, self.optional_name_label, nargs="?", const=DEFAULT_LABEL, help=TIME_ARGUMENTS_LABEL_HELP)
 
   def execute_command(self, parse_result):
     return self._get_subcommand_mapping()[parse_result.command](parse_result)
@@ -31,7 +36,10 @@ class TimeParser(AbstractParser):
     result = []
     for item in parse_result.time:
       item_ready = self._forward_number_from_argparse(item)
-      result += [construct_uniform_time_string(item_ready)]
+      if not parse_result.label:
+        result += [construct_uniform_time_string(item_ready)]
+      else:
+        result += [construct_uniform_time_string(item_ready, parse_result.label)]
 
     return '\n'.join(result)
 
