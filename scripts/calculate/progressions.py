c0c934f700a3543f1c3d868d7beb3e702759e6bc
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-02-16 03:04:09 +0200

    [0.6.5] integrating counter.py into Purple Tools:
    - Refactored the binary search algorithm, encapsulated calculations into their own functions and removed redundant/unnecessary code;
    - TODO: error checking for non-positive step still needs to be implemented;
    - TODO: The progression calculating algorithm itself still needs to be implemented;
    - TODO: implemeted calculations need to be integrated into the Purple Tools/argparse infrastructure;
    - TODO: add at least one more unit test for searching for one of the rightmost elements of a very big progression.

--- scripts/calculate/progressions.py
+++ scripts/calculate/progressions.py
@@ -19,45 +19,43 @@ def calculate_sum_of_arithmetic_progression(first_num, last_num):
 def find_progression_element_satisfying_condition(lower_bound, upper_bound, step, condition):
   _check_args_of_binary_search(lower_bound, upper_bound, step, condition)
 
-  current_index = (upper_bound - lower_bound) // step + 1
-  current_index = current_index // 2 if (current_index % 2 == 0) else current_index // 2 + 1
-  current_value = (current_index - 1) * step + lower_bound
+  current_index = _get_initial_index(lower_bound, upper_bound, step)
+  (current_index, current_value) = _bisect_once(current_index, lower_bound, step)  
   current_condition = condition(current_value)
   current_lower = lower_bound
-  current_upper = upper_bound
   absolute_index = current_index
-  _dump(current_index, absolute_index, current_value, current_lower, current_upper, current_condition)
+
   while current_condition != BinaryResult.OK:
     current_condition = condition(current_value)
+    
     if current_condition == BinaryResult.MOVE_LEFT:
-      current_value = (current_index - 1) * step + current_lower
-      current_upper = current_value
-      # current_index = (current_upper - current_lower) // step + 1
-      even_index = current_index % 2 == 0
-      current_index = current_index // 2 if even_index else current_index // 2 + 1
-      absolute_index = absolute_index - current_index if even_index else absolute_index - (current_index - 1)
-      current_value = (current_index - 1) * step + current_lower
-      _dump(current_index, absolute_index, current_value, current_lower, current_upper, current_condition)
+      old_index = current_index
+      (current_index, current_value) = _bisect_once(current_index, current_lower, step)
+      absolute_index = _advance_absolute_index(absolute_index, current_index, current_condition, old_index)
+
     elif current_condition == BinaryResult.MOVE_RIGHT:
-      # current_index += 1
-      # current_value = (current_index - 1) * step + current_lower
-      current_lower = ((current_index + 1) - 1) * step + current_lower # current_value
-      # current_index = (current_upper - current_lower) // step + 1
-      current_index = current_index // 2 if (current_index % 2 == 0) else current_index // 2 + 1
-      absolute_index += current_index
-      current_value = (current_index - 1) * step + current_lower
-      _dump(current_index, absolute_index, current_value, current_lower, current_upper, current_condition)
+      current_lower = _get_value_by_index(current_index + 1, current_lower, step) 
+      (current_index, current_value) = _bisect_once(current_index, current_lower, step)
+      absolute_index = _advance_absolute_index(absolute_index, current_index, current_condition)
 
   return absolute_index - 1
 
+def _advance_absolute_index(current_absolute_index, current_index, current_condition, old_index = 0):
+  if current_condition == BinaryResult.MOVE_RIGHT:
+    return current_absolute_index + current_index
+  elif current_condition == BinaryResult.MOVE_LEFT:
+    return current_absolute_index - current_index if old_index % 2 == 0 else current_absolute_index - (current_index - 1)
 
-# def _get_pair(lower_bound, upper_bound, step):
-#   current_index = (upper_bound - lower_bound) / step + 1
-#   current_value = (current_index - 1) * step + lower_bound
-#   return (current_index, current_value)
+def _get_initial_index(lower_bound, upper_bound, step):
+  return (upper_bound - lower_bound) // step + 1
 
-def _dump(current_index, absolute_index, current_value, current_lower, current_upper, current_condition):
-  print(f"\n----\nindex: {current_index}\nabsolute index: {absolute_index}\nvalue: {current_value}\nlower: {current_lower}\nupper: {current_upper}\ncondition: {current_condition}\n----\n")
+def _get_value_by_index(current_index, lower_bound, step):
+  return (current_index - 1) * step + lower_bound
+
+def _bisect_once(current_index, lower_bound, step):
+  new_index = current_index // 2 if (current_index % 2 == 0) else current_index // 2 + 1
+  new_value = _get_value_by_index(new_index, lower_bound, step)
+  return (new_index, new_value)
 
 def _check_args_of_binary_search(lower_bound, upper_bound, step, condition):
   if(upper_bound < lower_bound):
