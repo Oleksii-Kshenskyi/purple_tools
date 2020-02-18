73a62fe671e4ba2e5bc257fc3e752ca659510708
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2020-02-18 04:03:54 +0200

    [0.7.1] implementing date difference calculation:
    - Commented out the year 0001 assertion;
    - It seems like even the ISO %G flag outputs non-zero-padded years on some Linux distros. There seems to be no portable solution with strftime for the moment.

--- test/exploratory/etypes/edate/test_date_mechanics.py
+++ test/exploratory/etypes/edate/test_date_mechanics.py
@@ -12,7 +12,12 @@ class TestDateMechanics(unittest.TestCase):
 
     self.assertEqual("02/18/20", date(year = 2020, month = 2, day = 18).strftime("%m/%d/%y"))
     self.assertEqual("02/18/2020", date(year = 2020, month = 2, day = 18).strftime("%m/%d/%Y"))
-    self.assertEqual("01/01/0001", date(year = 1, month = 1, day = 1).strftime("%m/%d/%G"))
+
+    # this is not portable and depends on the libc strftime implementation.
+    # on Windows and OS X, this passes.
+    # On some Linux distros, it gives an assertion '1 != 0001' as apparently on some Linux distros strftime doesn't zero-pad years.
+    # Seems like I can't depend on strftime while outputting the resulting dates in the date module, this will have to be implemented manually.
+    # self.assertEqual("01/01/0001", date(year = 1, month = 1, day = 1).strftime("%m/%d/%Y"))
     
     self.assertEqual("9999-01-01", date(year = 9999, month = 1, day = 1).isoformat())
     with self.assertRaises(ValueError):
